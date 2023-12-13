40b4946092101d7309293ad32232c2e7e64469c8
Samsul Islam <ranafge@gmail.com>
2023-12-13 20:49:14 +0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-
-
 struct Book {
     title: String,
 }
@@ -11,18 +9,14 @@ impl<'a> Library<'a> {
     fn add_book(&mut self, book: &'a Book) -> String{
         self.book = book;
         println!("{} book is added", self.book.title);
-        self.book.title.clone()
-      
+        self.book.title.clone()      
     }
-
 }
-
 
 fn main() {
    let book = Book{title: "Book title".to_string()};
    let mut library = Library{
     book:&book
    };
-
    println!("{:?}", Library::add_book(&mut library, &book));
 }
